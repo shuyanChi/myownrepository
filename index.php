92937288776ac62f03968f2b593975362d08f2af
shuyan <schi@csumb.edu>
2018-12-28 17:20:39 +0000

    add cat photo

--- index.php
+++ index.php
@@ -38,22 +38,10 @@
 </head>
 <body>
     <?php
-        echo "<h1>Shuyan Chi CST336 site</h1>";
+        echo "<h1>Shuyan Workspace</h1>";
     ?>
-    <div class = "list">
-        <h2>labs</h2>
-        <ul>
-            <li><a href = "/cst336/labs/portfolio">Portfolio</a></li>
-            <li><a href ="/cst336/labs/lab2">lab2</a></li>
-        </ul>
-    </div>
-    <div class = "list">
-        <h2>hw</h2>
-        <ul>
-            <li><a href = "/cst336/hw/hw1">hw1</a></li>
-        </ul>
-    </div>
+
     <figure>
-        <img src = "/img/Russan Blue.jpg"  alt = "cat"/>
+        <img src = "img/cat.jpg"  alt = "cat"/>
     </figure>
 </body>
