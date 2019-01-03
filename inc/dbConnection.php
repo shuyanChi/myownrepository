8e95782822d22b8593241eb986b09ce91ee58f1c
shuyan <schi@csumb.edu>
2019-01-03 16:44:09 +0000

    update dbConnection file

--- inc/dbConnection.php
+++ inc/dbConnection.php
@@ -4,7 +4,7 @@
         $host = "localhost";
         $username = "root";
         $password ="";
-        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        //$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         
         //checks whether the URL contains "herokuapp" (using Heroku)
         if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
@@ -14,9 +14,39 @@
            $username = $url["user"];
            $password = $url["pass"];
         }
-        
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbConn;
     }
    
+
+
+/*<?php
+
+function startConnection($dbname="ottermart") {
+    //Creating database connection
+    $host = "localhost";
+//  $dbname = "ottermart";
+    $username = "root";
+    $password = "";
+    
+    //checks whether the URL contains "herokuapp" (using Heroku)
+    if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+       $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+       $host = $url["host"];
+       $dbname = substr($url["path"], 1);
+       $username = $url["user"];
+       $password = $url["pass"];
+    }
+    
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}
+?>
+
+*/
+
+
 ?>
